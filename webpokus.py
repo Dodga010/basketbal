49a9df84e9311aca27464855548fce896bb7725a
Dodga010 <dolezal45@gmail.com>
2025-02-24 21:23:57 +0100

    Update webpokus.py

--- webpokus.py
+++ webpokus.py
@@ -377,10 +377,10 @@ def main():
 
     elif page == "Shot Chart":
         st.subheader("🎯 Player Shot Chart")
-    players = fetch_players()
-    if not players:
-        st.warning("No player data available.")
-    else:
+    	players = fetch_players()
+    	if not players:
+        	st.warning("No player data available.")
+    	else:
         player_name = st.selectbox("Select a Player", players)
         generate_shot_chart(player_name)
 
